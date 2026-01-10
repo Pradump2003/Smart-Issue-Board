69b8fa5f4b9ff3c1fb63a005f80449bfeeb13355
Pradump2003 <pradump2003@gmail.com>
2026-01-10 18:42:21 +0530

    a

--- client/src/page/Login.jsx
+++ client/src/page/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import useFetchApi from "../hooks/useFetchApi";
 import { useUser } from "../hooks/useUser";
 import { toast } from "react-toastify";
@@ -11,8 +11,6 @@ const Login = () => {
   });
   const { fetchApi, loading, error } = useFetchApi();
   const { setUser } = useUser();
-
-  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -30,8 +28,7 @@ const Login = () => {
 
       if (res?.success) {
         toast.success("Login Successful");
-        // window.location.href = "/issue"; 
-        navigate("/issue");
+        window.location.href = "/issue"; 
       }
     } catch (err) {
       toast.error(err.message || "Login failed");
